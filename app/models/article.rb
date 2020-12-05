583f9fa8f20b70f26b121558844b3dd7f7a1c8b7
Danke1732 <s0lfa1732@gmail.com>
2020-12-05 22:59:33 +0900

    commentモデル及びcommentテーブルの作成(アソシエーション設定)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  has_many :comments, dependent: :destroy
 
   with_options presence: true do
     validates :image
