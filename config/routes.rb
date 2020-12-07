45dea2194db27328ed0a0cedd700a79692c8ca73
Danke1732 <s0lfa1732@gmail.com>
2020-12-07 17:01:22 +0900

    コメント削除機能ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   devise_for :users
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
     collection do
       get 'get_category_children/:id' => 'articles#get_category_children'
       get 'get_category_grandchildren/:id' => 'articles#get_category_grandchildren' 
