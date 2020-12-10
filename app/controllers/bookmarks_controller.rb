dfd61239cf6cc1fd73b2ec842a46e0ab28ed4ed1
Danke1732 <s0lfa1732@gmail.com>
2020-12-10 18:27:25 +0900

    rubocopにてコード修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,11 +8,11 @@ class BookmarksController < ApplicationController
       bookmark.save
     end
     bookmark_after = Bookmark.find_by(article_id: params[:id], user_id: current_user.id)
-    if bookmark_after
-      registration = "登録済み"
-    else
-      registration = "登録する"
-    end
+    registration = if bookmark_after
+                     '登録済み'
+                   else
+                     '登録する'
+                   end
     render json: { bookmark: bookmark_after, registration: registration }
   end
 end
