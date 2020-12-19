83abfdc37445938bbfdeee186edc094a62ef1c8a
Danke1732 <s0lfa1732@gmail.com>
2020-12-19 23:43:22 +0900

    profileモデル及びテーブルの作成(アソシエーションの設定)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_103245) do
+ActiveRecord::Schema.define(version: 2020_12_19_032524) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,16 @@ ActiveRecord::Schema.define(version: 2020_12_10_103245) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "hobby"
+    t.string "favorite_word"
+    t.text "introduction"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -99,4 +109,5 @@ ActiveRecord::Schema.define(version: 2020_12_10_103245) do
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
   add_foreign_key "notes", "users"
+  add_foreign_key "profiles", "users"
 end
