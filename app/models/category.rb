9f3cec9ad8ca054cf85b89de67490374a60bc594
Danke1732 <s0lfa1732@gmail.com>
2021-01-08 15:27:03 +0900

    カテゴリー検索詳細表示機能のモデルへメソッド追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,20 @@
 class Category < ApplicationRecord
   has_many :articles
   has_ancestry
+
+  def set_articles
+    if self.root?
+      start_id = self.indirects.first.id
+      end_id = self.indirects.last.id
+      articles = Article.where(category_id: start_id..end_id)
+      return articles
+    elsif self.has_children?
+      start_id = self.children.first.id
+      end_id = self.children.last.id
+      articles = Article.where(category_id: start_id..end_id)
+      return articles
+    else
+      return self.articles
+    end
+  end
 end
