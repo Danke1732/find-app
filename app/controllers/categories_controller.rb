fc94b8b393faf15b49afb2b9b5b6b272bd5cd5e1
Danke1732 <s0lfa1732@gmail.com>
2021-01-09 15:59:16 +0900

    カテゴリー検索一覧表示機能コントローラー実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: :show
 
+  def index
+    @parent_categories = Category.where(ancestry: nil)
+  end
+
   def show
     articles = @category.set_articles
     @articles = articles.preload(:user).order('updated_at DESC').with_attached_image.page(params[:page]).per(8)
