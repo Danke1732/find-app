dc62f032fde774e7f2c5ff45e556c6fec6e1b4a7
Danke1732 <s0lfa1732@gmail.com>
2021-02-15 22:55:53 +0900

    ビューファイル一部修正(ヘッダーハンバーガーメニュークリック時の挙動変更)

--- app/javascript/responsive_menu.js
+++ app/javascript/responsive_menu.js
@@ -1,14 +1,21 @@
 function responsiveAction() {
   const menuIcon = document.querySelector('.menu-icon');
   const menuBox = document.querySelector('.responsive-menu-area');
+  const mask = document.getElementById('mask');
   const closeMenu = document.querySelector('.menu-close');
 
   menuIcon.addEventListener('click', () => {
     menuBox.classList.toggle('visible');
+    mask.classList.toggle('hidden');
   });
 
   closeMenu.addEventListener('click', () => {
     menuBox.classList.remove('visible');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    closeMenu.click();
   });
 };
 addEventListener('DOMContentLoaded', responsiveAction);
